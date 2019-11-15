ca37187bd3fdf862dfb329113960b8400449ff2b
Asen <githuba@abv.bg>
2019-11-15 21:58:54 +0200

    Initial commit

---
+++ src/main/java/asen/restswagger/RestSwaggerApplication.java
@@ -0,0 +1,13 @@
+package asen.restswagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSwaggerApplication.class, args);
+	}
+
+}
